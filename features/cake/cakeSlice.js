b2641d26fc063d7304d94fb3db3621cb2b138463
amontalvof <amontalvof92@gmail.com>
2023-01-22 13:18:31 -0600

    feat: Cake Slice

---
+++ features/cake/cakeSlice.js
@@ -0,0 +1,23 @@
+const { createSlice } = require('@reduxjs/toolkit');
+
+const initialState = {
+    numOfCakes: 10,
+};
+
+const cakeSlice = createSlice({
+    name: 'cake',
+    initialState,
+    reducers: {
+        ordered: (state) => {
+            state.numOfCakes--;
+        },
+        restocked: (state, action) => {
+            state.numOfCakes += action.payload;
+        },
+    },
+});
+
+module.exports = {
+    cakeReducer: cakeSlice.reducer,
+    cakeActions: cakeSlice.actions,
+};
